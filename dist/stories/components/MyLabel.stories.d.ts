ceb898c6b13babfce4c11835aa657c29fb448bed
ferdeolazabal <ferdeolazabal@gmail.com>
2022-05-08 14:27:07 -0300

    Limpieza de Cache

--- dist/stories/components/MyLabel.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-declare const _default: ComponentMeta<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
-export default _default;
-export declare const Basic: ComponentStory<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
-export declare const AllCaps: ComponentStory<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
-export declare const Secondary: ComponentStory<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
-export declare const Tertiary: ComponentStory<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
-export declare const CustomFontColor: ComponentStory<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
-export declare const CustomBackgroundColor: ComponentStory<({ label, size, AllCaps, color, fontColor, backgroundColor }: import("../../components/MyLabel").IMyLabelProps) => JSX.Element>;
